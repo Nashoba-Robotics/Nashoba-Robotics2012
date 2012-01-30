ce0ec0596afca83aa69713c9be08c97ce63892b2
Alex <alex_brinister@yahoo.com>
2012-01-30 16:06:04 -0500

    Created BallIntakeCommand h and cpp files.

---
+++ Commands/BallIntakeCommand.cpp
@@ -0,0 +1,32 @@
+#include "BallIntakeCommand.h"
+#include "../Debug.h"
+
+BallIntakeCommand::BallIntakeCommand() : CommandBase ("BallIntakeCommand")
+{
+
+}
+
+void BallIntakeCommand::Initialize()
+{
+
+}
+
+void BallIntakeCommand::Execute()
+{
+
+}
+
+bool BallIntakeCommand::IsFinished()
+{
+	return true;
+}
+
+void BallIntakeCommand::End()
+{
+
+}
+
+void BallIntakeCommand::Interrupted()
+{
+
+}
---
+++ Commands/BallIntakeCommand.h
@@ -0,0 +1,19 @@
+#ifndef BALLINTAKECOMMAND_H_
+#define BALLINTAKECOMMAND_H_
+
+#include "../CommandBase.h"
+#include "../Subsystems/BallIntakeSubsystem.h"
+
+class BallIntakeCommand : public CommandBase
+{
+private:
+public:
+	BallIntakeCommand();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual void IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
